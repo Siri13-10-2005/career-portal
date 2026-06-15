4736f3415ffbd21e2d0ce5fc56c029288673a243
Siri A K <siriammathi13@gmail.com>
2026-06-16 02:09:43 +0530

    Fix CORS for deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -13,6 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173"
+        "https://career-portal-lxc5.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
